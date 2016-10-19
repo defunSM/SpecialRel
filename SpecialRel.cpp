7966e0063f6a9938214e08073f19a169c14ed297
SM Salman Hossain <sxh8083@g.rit.edu>
2016-10-18 21:07:33 -0400

    New additions

--- SpecialRel.cpp
+++ SpecialRel.cpp
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
+#include <math.h>
+
 
 // Still need to work on this. Just empty right now.
 
+const float c = 299792458;
+
+float cToMeter(float x){
+
+  return x * c;
+
+}
+
+float meterToC(float x) {
+
+  return x / c;
+
+}
+
 int main(int argc, char *argv[]) {
 
-
+  std::cout << cToMeter(0.5) << std::endl;
   return 0;
 }
